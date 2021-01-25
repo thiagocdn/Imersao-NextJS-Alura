4a1b7f8e7b6ad1418a174570d5d744879e327304
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-01-25 20:00:38 -0300

    Finished first day

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components'
 import db from '../db.json';
-
-
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
+import Footer from '../Components/Footer';
+import GithubCorner from '../Components/GithubCorner';
+import QuizBackground from '../Components/QuizBackground';
+import QuizLogo from '../Components/QuizLogo';
+import Widget from '../Components/Widget';
+import GitHubCorner from '../Components/GithubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -20,38 +18,29 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-const Widget = styled.div`
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
-  background-color: ${({ theme }) => {
-    return theme.colors.mainBg;
-  }};
-  border-radius: 4px;
-  overflow: hidden;
-  h1, h2, h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
-`;
-
 export default function Home() {
   return (
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg} >
       <QuizContainer>
         <Widget>
-          huahua  
+          <Widget.Header>
+            <h1>The legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum blablabla...</p> 
+          </Widget.Content> 
         </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Demais Quizzes...</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum blablabla...</p>  
+          </Widget.Content> 
+        </Widget>
+        <Footer />
       </QuizContainer>
-
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/thiagocdn"/>
+    </QuizBackground>
   );
 }
