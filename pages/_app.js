73f206639e656025e1eb00641186d281a5a0f0fa
Thiago Cedran Santos <thiagocdn@gmail.com>
2021-01-26 20:11:05 -0300

    Finished second day

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from '../db.json';
 
@@ -23,7 +24,7 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
 const theme = db.theme;
 
@@ -31,6 +32,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        </Head>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
 import Footer from '../Components/Footer';
-import GithubCorner from '../Components/GithubCorner';
 import QuizBackground from '../Components/QuizBackground';
-import QuizLogo from '../Components/QuizLogo';
 import Widget from '../Components/Widget';
 import GitHubCorner from '../Components/GithubCorner';
 
@@ -19,28 +20,44 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('a');
+
   return (
-    <QuizBackground backgroundImage={db.bg} >
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Alura Quiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>The legend of Zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem ipsum blablabla...</p> 
-          </Widget.Content> 
+            <form onSubmit="">
+              <input
+                placeholder="Diz ai seu nome..."
+                onChange={event => setName(event.target.value)}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Header>
             <h1>Demais Quizzes...</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem ipsum blablabla...</p>  
-          </Widget.Content> 
+            <p>Lorem ipsum blablabla...</p>
+          </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/thiagocdn"/>
+      <GitHubCorner projectUrl="https://github.com/thiagocdn" />
     </QuizBackground>
   );
 }
